b0749fdd1fe85f9deb837f4fc8524dc7b3bcccba
wilfreud <killlergangster@gmail.com>
2024-05-04 14:39:55 +0000

    feat: teacher controller+routes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: ".env.development" });
+
+console.table(process.env);
 
 // const envPath = `../env/.env.development`;
 const connectDB = async () => {
   try {
+    console.log("---------->", process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}...`); //
   } catch (error) {
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,12 +2,14 @@ import express from "express";
 import appRoutes from "./app.routes.js";
 import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
+import teacherRoutes from "./teacher.routes.js";
 
 const router = express.Router();
 
 router.use("/app", appRoutes);
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/teacher", teacherRoutes);
 
 //export default
 export default router;
