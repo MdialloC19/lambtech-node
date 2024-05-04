19e1720ea4b153704338280fccfe4edd8cb00c3a
MdialloC19 <mdiallo3211@gmail.com>
2024-05-04 09:12:23 +0000

    test-moussa

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import appRoutes from "./app.routes.js";
 import authRoutes from "./auth.routes.js";
+import userRoutes from "./user.routes.js";
 
 const router = express.Router();
 
 router.use("/app", appRoutes);
 router.use("/auth", authRoutes);
+router.use("/user", userRoutes);
 
 //export default
 export default router;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const router = express.Router();
+
+import { check } from "express-validator";
+// const usersControllers = require("../../controllers/usersControllers");
+/**
+ * @desc Route to get all users.
+ * @route GET api/users
+ * @access Public
+ */
+router.get("/", (req, res) => {
+  res.send("USER ROUTE");
+});
+
+/**
+ * @desc Route to register user
+ * @route POST api/users
+ * @access Public
+ */
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    console.log(req.body);
+    res.send("USER post");
+  }
+);
+
+export default router;
